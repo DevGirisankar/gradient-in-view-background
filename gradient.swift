23ba01ae9a081b5aac5bebc61595aa6d172ab661
Girisankar G <girisankar30@gmail.com>
2021-09-08 18:56:05 +0530

    Update gradient.swift

--- gradient.swift
+++ gradient.swift
@@ -1,10 +1,57 @@
-extension UIView {
-    func applyGradient(colours: [UIColor], horizontal:Bool = false) -> Void {
-        let gradient: CAGradientLayer = CAGradientLayer()
-        gradient.frame = self.bounds
-        gradient.colors = colours.map { $0.cgColor }
-        gradient.startPoint = horizontal ?  CGPoint(x: 0.0, y: 0.5) : CGPoint(x: 0.5, y: 0.0)
-        gradient.endPoint = horizontal ? CGPoint(x: 1.0, y: 0.5) : CGPoint(x: 0.5, y: 1.0)
-        self.layer.insertSublayer(gradient, at: 0)
+import UIKit
+
+@IBDesignable
+class GradientView: UIView {
+
+    override class var layerClass: AnyClass { return CAGradientLayer.self }
+
+    private var gradientLayer: CAGradientLayer { return layer as! CAGradientLayer }
+
+    @IBInspectable var color1: UIColor = .white { didSet { updateColors() } }
+    @IBInspectable var color2: UIColor = .blue  { didSet { updateColors() } }
+
+    @IBInspectable var startX: CGFloat = 0.0 {
+        didSet {
+            configureGradient()
+        }
     }
+
+    @IBInspectable var startY: CGFloat = 0.0 {
+        didSet {
+            configureGradient()
+        }
+    }
+
+    @IBInspectable var endX: CGFloat = 1.0 {
+        didSet {
+            configureGradient()
+        }
+    }
+
+    @IBInspectable var endY: CGFloat = 1.0 {
+        didSet {
+            configureGradient()
+        }
+    }
+
+    override init(frame: CGRect = .zero) {
+        super.init(frame: frame)
+        configureGradient()
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        configureGradient()
+    }
+
+    private func configureGradient() {
+        gradientLayer.startPoint = CGPoint(x: startX, y: startY)
+        gradientLayer.endPoint = CGPoint(x: endX, y: endY)
+        updateColors()
+    }
+
+    private func updateColors() {
+        gradientLayer.colors = [color1.cgColor, color2.cgColor]
+    }
+
 }
